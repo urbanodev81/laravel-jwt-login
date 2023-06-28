18d498a6e8c5c2d6592176f26dc860e0a7f3939b
ricardo.urbano <ricardo.urbano@cejam.org.br>
2023-06-28 15:10:17 -0300

    authenticação por jwt ok

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get(
+    '/',
+    function() {
+        return response()->json(['version' => app()->version()]);
+    }
+);
+
+Route::post('auth/login', [AuthController::class, 'login'])->name('auth.login');
